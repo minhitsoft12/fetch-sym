fdadf43f04f60cb954e042d1425bf40e0385dc6d
MinhDev <minhitsoft1209@gmail.com>
2021-06-12 22:33:27 +0700

    Herokudâsd

--- src/Controller/FetchController.php
+++ src/Controller/FetchController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class FetchController extends AbstractController
 {
     /**
-     * @Route("/fetch/{reactRouting}", name="home", defaults={"reactRouting": null})
+     * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
      */
     public function index()
     {
